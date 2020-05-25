7250f0176d84e7d1a380d78c8ea46e24911e1406
Maurício Dias <mau.dias@gmail.com>
2020-05-26 00:30:15 +0200

    Refactoring. Moved game functions from db.py to model

--- models/game.py
+++ models/game.py
@@ -1,6 +1,18 @@
 from basemodel import BaseModel
-from peewee import ForeignKeyField, IntegerField, TextField
+from loguru import logger
+from peewee import ForeignKeyField, IntegerField, TextField, DoesNotExist
 from player import Player
+import random
+import uuid
+
+def get_player(player_uuid):
+    logger.info(f'Searching player with UUID {player_uuid}')
+    player = Player.get_or_none(Player.uuid == player_uuid)
+    if player:
+        logger.info(f'Found player with id {player.id}')
+    else:
+        logger.info('No player found')
+    return player
 
 class Game(BaseModel):
     player1 = ForeignKeyField(Player, backref='games')
@@ -8,3 +20,63 @@ class Game(BaseModel):
     dice = IntegerField(default=-1)
     next_player = ForeignKeyField(Player, backref='games', null=True)
     board = TextField(default='')
+
+    @classmethod
+    def find_game(cls, player_uuid):
+        logger.info(f'Searching game with player {player_uuid}')
+        if (player := get_player(player_uuid)) is None:
+            return None
+        else:
+            game = cls.select().where((cls.player1 == player)|(cls.player2 == player)).get()
+            logger.info(f'Found game id {game.id}')
+        return game
+
+    @classmethod
+    def new_game(cls):
+        player = Player()
+        player_uuid = str(uuid.uuid4())
+        logger.info(f'Created new player. Attempting to assign UUID {player_uuid}')
+        while Player.get_or_none(uuid=player_uuid):
+            player_uuid = str(uuid.uuid4())
+            logger.info(f'UUID exists. Attempting to assign {player_uuid}')
+            continue
+        player.uuid = player_uuid
+        player.save()
+        logger.info('New player created')
+        try:
+            game = cls.select().where(cls.player2 == None).get()
+            logger.info(f'Found game id {game.id} with an empty slot. Assigning to player UUID {player.uuid}')
+            game.player2 = player
+        except cls.DoesNotExist:
+            logger.info('No available games found, creating new.')
+            game = cls(player1=player)
+        game.save()
+        logger.info(f'Game id {game.id} saved')
+        return game
+
+    @classmethod
+    def update_game(cls, data):
+        pass
+
+    @classmethod
+    def throw_dice(cls, player_uuid):
+        game = cls.get(player_uuid)
+        if game:
+            logger.info(f'Found game {game.id}')
+            logger.info(f'Current dice value: {game.dice}')
+            if game.dice > -1:
+                logger.info('Dice was played already, not changing')
+                return game.dice
+            else:
+                dice = []
+                for i in range(4):
+                    dice.append(random.randint(0,1))
+                game.dice = sum(dice)
+                logger.info(f'New dice value: {game.dice}')
+                logger.info('Saving new dice value')
+                game.save()
+                logger.info('Game updated with new dice value')
+                return game.dice
+        else:
+            logger.info(f'No game found for player with UUID {player_uuid}')
+            return None
--- models/player.py
+++ models/player.py
@@ -2,11 +2,22 @@ from basemodel import BaseModel
 from peewee import TextField, IntegerField
 
 class Player(BaseModel):
-    player_uuid = TextField()
-    rocks_left = IntegerField(default=7)
+    uuid = TextField()
+    total_rocks = IntegerField(default=7)
+    active_rocks = IntegerField(default=0)
 
     @classmethod
     def less_rocks(cls, player_uuid):
         player = cls.select().where(cls.player_uuid == player_uuid).get()
         player.rocks_left -= 1
         player.save()
+
+    @classmethod
+    def play_rock(cls, player_uuid):
+        player = cls.select().where(cls.player_uuid == player_uuid).get_or_none()
+        if player:
+            if player.active_rocks < player.total_rocks:
+                player.active_rocks += 1
+                player.save
+                return True
+        return False
